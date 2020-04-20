af3f31a33b289e9ec0b4464c2f45b1c4e1163311
shahrose <shahrose@shahrose-macbookpro3.roam.corp.google.com>
2020-04-20 21:19:02 +0500

    Updates

---
+++ precache-manifest.6e4cd2176063a4933ca293446ed74621.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d2199c123f37664f5cdbc003571d44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e73afe6e766f3bb343b8",
+    "url": "/static/css/main.1a498aef.chunk.css"
+  },
+  {
+    "revision": "f81e9a8506aa50416956",
+    "url": "/static/js/2.6fd58bbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6fd58bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e73afe6e766f3bb343b8",
+    "url": "/static/js/main.446dd7e8.chunk.js"
+  },
+  {
+    "revision": "a5acc6b7f2fbf17a3564",
+    "url": "/static/js/runtime-main.59a40d66.js"
+  },
+  {
+    "revision": "815048e745b45ddaa558171ee911c92c",
+    "url": "/static/media/me.815048e7.jpg"
+  }
+]);
